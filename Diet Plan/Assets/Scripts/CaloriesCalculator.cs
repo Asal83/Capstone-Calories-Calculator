eccf5380365edb58d2e25d56586e73d9c7d30c0a
Asal <pamela.m1984@gmail.com>
2019-09-20 14:46:16 -0600

    Calories Count Project

---
+++ Diet Plan/Assets/Scripts/CaloriesCalculator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CaloriesCalculator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TMP_InputField firstName;
+    public TMP_InputField lastName;
+    public TMP_InputField addChicken;
+    public TMP_InputField addrice;
+    public TMP_InputField addBeans;
+    public TMP_InputField addSteak;
+    public TextMeshProUGUI mytext;
+    const int chickencalories = 180;
+    const int SteakCalories = 190;
+    const int RiceCalories = 200;
+    const int BeansCalories = 120;
+
+
+    public void TotalMealCalories()
+    {
+        int totalFoodCalories = 0;
+        int AddChicken = int.Parse(addChicken.text);
+        int AddRice = int.Parse(addrice.text);
+        int AddBeans = int.Parse(addBeans.text);
+        int AddSteak = int.Parse(addSteak.text);
+        totalFoodCalories += AddChicken * chickencalories;
+        totalFoodCalories += (AddRice * chickencalories);
+        totalFoodCalories += (AddBeans * chickencalories);
+        totalFoodCalories += (AddSteak * chickencalories);
+        mytext.text = firstName.text +" " + lastName.text + totalFoodCalories.ToString();
+    }
+
+
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Diet Plan/Assets/Scripts/UI_System.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_System : MonoBehaviour
+{
+    UI_Pages PreviousPage;
+    UI_Pages CurrentPage;
+    void Start()
+    {
+        UI_Pages[] pages = GetComponentsInChildren<UI_Pages>(true);//when we set it True ,it gives us all the pages Active or disActive.
+        pages[0].gameObject.SetActive(true);//when I open the App on mobile I want the first page to be displayed.my code come I want all the pages be
+        //disactive and active the current page.
+        for (int i = 1; i < pages.Length; i++)
+        {
+            pages[i].gameObject.SetActive(false);
+        }
+        CurrentPage = pages[0];
+    }
+    public void SwitchPage(UI_Pages nextPage)
+    {//Disable the current page then enable the next page which is Page,we need a method to switch pages.it recives the page we are going to switch to 
+     //ex)we are in the second page and want to switch to first page,we Just need to know to which page are going to switch.
+     //I am going to diable the current page make it invisible then enable the next page make it appear which is .
+     //we first need to know what is the current page,for now I define a variable call current page.
+     //what do we need,we switching page staring from stating page to
+     //
+     //another page,say wind page.so we need a method to do that for us.It receives the page we are going to switch to .
+     //
+        PreviousPage = CurrentPage;//current page is the one when you run the program the first element.
+        CurrentPage.gameObject.SetActive(false);
+        nextPage.gameObject.SetActive(true);
+        CurrentPage = nextPage;
+    }
+    public void GoToPreviousPage()
+    {
+        SwitchPage(PreviousPage);//for Back Button.
+    }
+
+}
