4b8c453867ba519cd058ae845fd17320e6b2154f
Asal <pamela.m1984@gmail.com>
2019-09-20 18:38:14 -0600

    add youtube Link

---
+++ Diet Plan/Diet Plan/Assets/Scripts/Video.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Video : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void openyoutube()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=vl5_DH9L-Kc");
+    }
+    public void tahchinYoutube()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=91T8RR2-wi8");
+    }
+    public void kashkBadenjoonYoutube()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=RlB1MF60CmM");
+    }
+    public void khoroshtSabzi()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=6BL6ehObz0s");
+    }
+}
